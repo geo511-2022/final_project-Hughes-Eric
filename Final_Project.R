fa7adabf875a4386227f49f1a924420bfb6384dd
Eric Hughes <erichugh@buffalo.edu>
2022-11-08 20:21:08 -0500

    commit

--- Final_Project.R
+++ Final_Project.R
@@ -10,37 +10,40 @@ library(ggmap)
 raw_median_income <- read.csv(file = "C:/Users/17166/OneDrive - University at Buffalo/Desktop/Fall Semester 2022/GEO 511/Final Project/median household income2020 census/ACSST5Y2020.S1903-Data.csv",
                               skip = 2, header = F)
 median_household_income <- raw_median_income[c(2,3,324)]
-colnames(median_household_income) <- c("GEOID", "Census Tract", "Median Household Income")
+colnames(median_household_income) <- c("GEOID", "Census.Tract", "Median.Household.Income")
 median_household_income$GEOID = as.character(median_household_income$GEOID)
+
+#Buffalo Boundary
+Buffalo_border <- st_read("C:/Users/17166/OneDrive - University at Buffalo/Desktop/Fall Semester 2022/GEO 511/Final Project/Buff_border_shape") %>%
+  st_transform(4326)
+Buffalo_sp <- as_Spatial(st_transform(Buffalo_border, 4326))
+Buffalo_bbox <- Buffalo_sp@bbox
 
 #import census tract shapefile and merge median income
 census_tract_2020 <- st_read("C:/Users/17166/OneDrive - University at Buffalo/Desktop/Fall Semester 2022/GEO 511/Final Project/2020_NY_census_tract") %>%
-  filter(COUNTYFP == "029")
+  filter(COUNTYFP == "029") 
 
 median_income_shape <- census_tract_2020 %>% left_join(median_household_income, by = c("GEOID" = "GEOID"))
-median_income_shape$INTPTLATs = as.numeric(median_income_shape$INTPTLAT)
+median_income_shape$INTPTLAT = as.numeric(median_income_shape$INTPTLAT)
 median_income_shape$INTPTLON = as.numeric(median_income_shape$INTPTLON)
-#median_income_shape$`Median Household Income` = as.integer(median_income_shape$`Median Household Income`)
-median_income_shape$`Median Household Income`=as.numeric(levels(median_income_shape$`Median Household Income`))[median_income_shape$`Median Household Income`]
+median_income_shape$`Median.Household.Income` = as.integer(median_income_shape$`Median.Household.Income`)
 
 #convert census tracts to centroids
 
 nc <- median_income_shape %>% 
-  st_transform(2262)
-nc$`Median Household Income` = as.numeric(nc$`Median Household Income`)
+  st_transform(4326) %>%
+  st_intersection(Buffalo_border)
+
 sp_cent <- gCentroid(as(nc, "Spatial"), byid = TRUE)
 
 sf_cent <- st_centroid(nc) 
 
 #Buffalo Supermarkets
 
-Buffalo_supermarkets_csv <- st_read("C:/Users/17166/OneDrive - University at Buffalo/Desktop/Fall Semester 2022/GEO 511/Final Project/Buffalo_Supermarkets/Buffalo_Supermarkets.csv")
-buffalo_supermarkets <- geocode(Buffalo_supermarkets_csv$Address, output = c("latlon", "latlona", "more", "all"), source = c("google", "dsk"))
+#Buffalo_supermarkets_csv <- st_read("C:/Users/17166/OneDrive - University at Buffalo/Desktop/Fall Semester 2022/GEO 511/Final Project/Buffalo_Supermarkets/Buffalo_Supermarkets.csv")
+#buffalo_supermarkets <- geocode(Buffalo_supermarkets_csv$Address, output = c("latlon", "latlona", "more", "all"), source = c("google", "dsk"))
 
-#Buffalo Boundary
-Buffalo_border <- st_read("C:/Users/17166/OneDrive - University at Buffalo/Desktop/Fall Semester 2022/GEO 511/Final Project/Buff_border_shape")
-Buffalo_sp <- as_Spatial(Buffalo_border)
-Buffalo_bbox <- Buffalo_sp@bbox
+
 
 # Download the basemap
 basemap <- get_stamenmap(
@@ -49,7 +52,7 @@ basemap <- get_stamenmap(
   maptype = "toner-lite")
 
 #Median Income Plot
-median_income_plot <- ggplot(census_tract_2020) + 
+median_income_plot <- ggplot(median_income_shape) + 
   geom_sf(data = nc, fill = 'white') + 
   geom_sf(data = sp_cent %>% st_as_sf, color = 'blue') + 
   geom_sf(data = sf_cent, color = 'red')+
@@ -58,7 +61,7 @@ median_income_plot <- ggplot(census_tract_2020) +
 
 
 ggmap(basemap) + 
-  geom_point(data = nc, aes(x = INTPTLON, y = INTPTLAT, color = "Median Household Income"), 
-             size = .025, alpha = 0.7) +
-  scale_color_gradient(low = "light green", high = "dark green")
+  geom_sf(data = nc, aes( fill = Median.Household.Income), 
+             size = .025, alpha = 0.7, inherit.aes = F) +
+  scale_fill_gradient(low = "light green", high = "dark green")
 
